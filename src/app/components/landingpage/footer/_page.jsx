c81efd75544f6c515867da2cc29e85a868427bcc
Ujjwal Katiyar <ukcode07@gmail.com>
2024-10-07 20:39:29 +0530

    folder distributed

---
+++ src/app/components/landingpage/footer/_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const _page = () => {
+  return (
+    <div>_page</div>
+  )
+}
+
+export default _page
---
+++ src/app/components/landingpage/navbar/_page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const _page = () => {
+  return (
+    <div>_page</div>
+  )
+}
